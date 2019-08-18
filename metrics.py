3a879b257c169b618dae12ef2e7ecdf3c9ff15a8
wanjun <328518864@qq.com>
2019-08-18 13:46:27 +0800

    add LSA and DSA

--- metrics.py
+++ metrics.py
@@ -6,6 +6,8 @@ import pandas as pd
 from keras import Model
 from tqdm import tqdm
 from keras.models import load_model
+from scipy import stats
+from functools import reduce
 
 
 ## deep gauge
@@ -384,11 +386,116 @@ class nac(object):
         return np.argsort(np.sum(self.neuron_activate>self.t,axis=1))[::-1]
 
 
-## ours
+## deepgini
 def deep_metric(pred_test_prob):
     metrics=np.sum(pred_test_prob**2,axis=1)
     rank_lst=np.argsort(metrics)
     return rank_lst
+
+## Surprise Adequacy
+
+## LSA
+class LSA(object):
+    def __init__(self,train,input,layers,std=0.05):
+        '''
+        train:训练集数据
+        input:输入张量
+        layers:输出张量层
+        '''
+        self.train=train
+        self.input=input
+        self.layers=layers
+        self.std=std
+        self.lst=[]
+        self.std_lst=[]
+        self.mask=[]
+        self.neuron_activate_train=[]
+        index_lst=[]
+
+        for index,l in layers:
+            self.lst.append(Model(inputs=input,outputs=l))
+            index_lst.append(index)
+            i=Model(inputs=input,outputs=l)
+            if index=='conv':
+                temp=i.predict(train).reshape(len(train),-1,l.shape[-1])
+                temp=np.mean(temp,axis=1)
+            if index=='dense':
+                temp=i.predict(train).reshape(len(train),l.shape[-1])
+            self.neuron_activate_train.append(temp.copy())
+            self.std_lst.append(np.std(temp,axis=0))
+            self.mask.append((np.array(self.std_lst)>std))
+        self.neuron_activate_train=np.concatenate(self.neuron_activate_train,axis=1)
+        self.mask=np.concatenate(self.mask,axis=0)
+        #self.lst=list(zip(index_lst,self.lst))
+
+    def fit(self,test,use_lower=False):
+        self.neuron_activate_test=[]
+        for index,l in self.lst:
+            if index=='conv':
+                temp=l.predict(test).reshape(len(test),-1,l.output.shape[-1])
+                temp=np.mean(temp,axis=1)
+            if index=='dense':
+                temp=l.predict(test).reshape(len(test),l.output.shape[-1])
+            self.neuron_activate_test.append(temp.copy())
+        self.neuron_activate_test=np.concatenate(self.neuron_activate_test,axis=1)
+        test_score = []
+        for test_sample in self.neuron_activate_test[:,self.mask]:
+            test_mean = np.zeros_like(test_sample)
+            for train_sample in self.neuron_activate_train[:,self.mask]:
+                temp = test_sample-train_sample
+                kde = stats.gaussian_kde(temp, bw_method='scott')
+                test_mean+=kde.evaluate(temp)
+            test_score.append(reduce(lambda x,y:np.log(x)+np.log(y),test_mean/len(self.neuron_activate_train)))
+        return test_score
+
+## DSA
+class DSA(object):
+    def __init__(self,train,label,input,layers,std=0.05):
+        '''
+        train:训练集数据
+        input:输入张量
+        layers:输出张量层
+        '''
+        self.train=train
+        self.input=input
+        self.layers=layers
+        self.std=std
+        self.lst=[]
+        self.std_lst=[]
+        self.mask=[]
+        self.neuron_activate_train=[]
+        index_lst=[]
+
+        for index,l in layers:
+            self.lst.append(Model(inputs=input,outputs=l))
+            index_lst.append(index)
+            i=Model(inputs=input,outputs=l)
+            if index=='conv':
+                temp=i.predict(train).reshape(len(train),-1,l.shape[-1])
+                temp=np.mean(temp,axis=1)
+            if index=='dense':
+                temp=i.predict(train).reshape(len(train),l.shape[-1])
+            self.neuron_activate_train.append(temp.copy())
+        self.neuron_activate_train=np.concatenate(self.neuron_activate_train,axis=1)
+        self.train_label = np.array(label)
+
+    def fit(self,test,label,use_lower=False):
+        self.neuron_activate_test=[]
+        for index,l in self.lst:
+            if index=='conv':
+                temp=l.predict(test).reshape(len(test),-1,l.output.shape[-1])
+                temp=np.mean(temp,axis=1)
+            if index=='dense':
+                temp=l.predict(test).reshape(len(test),l.output.shape[-1])
+            self.neuron_activate_test.append(temp.copy())
+        self.neuron_activate_test=np.concatenate(self.neuron_activate_test,axis=1)
+        test_score = []
+        for test_sample,label_sample in zip(self.neuron_activate_test,label):
+            dist_a = np.min(((self.neuron_activate_train[self.train_label == label_sample,:]-test_sample)**2).sum(axis=1))
+            dist_b = np.min(((self.neuron_activate_train[self.train_label != label_sample,:]-test_sample)**2).sum(axis=1))
+            test_score.append(dist_a/dist_b)
+        return test_score
+
 
 if __name__=='__main__':
     pass
